7b317792b19e2214022f7f02b71dc3754f4b900b
alex <alex_is_ok@yahoo.com>
2019-02-21 19:15:34 +0200

    bot edited

--- app.py
+++ app.py
@@ -4,34 +4,27 @@ from pymessenger.bot import Bot
 
 app = Flask(__name__)
 
-ACCESS_TOKEN = 'YOUR_ACCESS_TOKEN'
+ACCESS_TOKEN = 'EAADgKyefUVgBADfuFA7oacTa82giKVhtMPqjljc0gAYTi2zcFB3esphOhRUYpZBwQnFdgZCg20WDTSzgdMPbh6xPwY8bsjJDiSjLyg17YZCrEpDOswL9nNVMwS0XnDZABXtRavwtvjBUErZAi0g4gLhoWcuz02EJTFxOkXgHtV262NGfaatrQ'
 VERIFY_TOKEN = 'YOUR_VERIFY_TOKEN'
 
 bot = Bot(ACCESS_TOKEN)
 
 
-# Получать сообщения, посылаемые фейсбуком нашему боту мы будем в этом терминале вызова
 @app.route('/', methods=['GET', 'POST'])
 def receive_message():
     if request.method == 'GET':
-        # до того как позволить людям отправлять что-либо боту, Facebook проверяет токен,
-        # подтверждающий, что все запросы, получаемые ботом, приходят из Facebook
         token_sent = request.args['hub.verify_token']
         return verify_fb_token(token_sent)
-    # если запрос не был GET, это был POST-запрос и мы обрабатываем запрос пользователя
     else:
-        # получаем сообщение, отправленное пользователем для бота в Facebook
         output = request.get_json()
         for event in output['entry']:
             messaging = event['messaging']
             for message in messaging:
                 if message.get('message'):
-                    # определяем ID, чтобы знать куда отправлять ответ
                     recipient_id = message['sender']['id']
                 if message['message'].get('text'):
                     response_sent_text = get_message()
                     send_message(recipient_id, response_sent_text)
-                # если пользователь отправил GIF, фото, видео и любой не текстовый объект
                 if message['message'].get('attachments'):
                     response_sent_nontext = get_message()
                     send_message(recipient_id, response_sent_nontext)
@@ -39,8 +32,6 @@ def receive_message():
 
 
 def verify_fb_token(token_sent):
-    '''Сверяет токен, отправленный фейсбуком, с имеющимся у вас.
-    При соответствии позволяет осуществить запрос, в обратном случае выдает ошибку.'''
     if token_sent == VERIFY_TOKEN:
         return request.args['hub.challenge']
     else:
@@ -48,15 +39,13 @@ def verify_fb_token(token_sent):
 
 
 def send_message(recipient_id, response):
-    '''Отправляет пользователю текстовое сообщение в соответствии с параметром response.'''
     bot.send_text_message(recipient_id, response)
     return 'Success'
 
 
 def get_message():
-    '''Отправляет случайные сообщения пользователю.'''
     sample_responses = ["Потрясающе!", "Я вами горжусь!", "Продолжайте в том же духе!",
-                        "Лучшее, что я когда-либо видел!"]
+                        "Лучшее, что я когда-либо видел!", "Валера, это ты?"]
     return random.choice(sample_responses)
 
 
